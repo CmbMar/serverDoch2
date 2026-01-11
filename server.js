a6dff2b9cef28d3aacc9d7454d4f3d0bcb9d1e9b
CmbMar_HP <martin.strejcek@seznam.cz>
2026-01-11 19:46:03 +0100

    key Bearer

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json({ limit: '50mb' })); // Zvýšený limit pro přenos komple
  */
 const authenticate = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader || authHeader !== `${SERVER_API_KEY}`) {
+  if (!authHeader || authHeader !== `Bearer ${SERVER_API_KEY}`) {
     console.warn(`[${new Date().toISOString()}] Auth: Neautorizovaný přístup odmítnut.`);
     return res.status(401).json({ error: 'Unauthorized' });
   }
